7266266c81d152153d489af80b95635e3abcb0c2
Dan Indre <indre.dan96@gmail.com>
2021-06-25 10:37:04 +0300

    Add db connection

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ import logger from './logger'
 import { IS_PRODUCTION, PORT } from './config'
 
 import sampleRouter from './controller/v1/sampleController'
-;(() => {
+import { connectDatabase } from './database'
+connectDatabase().then(async connection => {
   const corsOptions: CorsOptions = {
     origin: IS_PRODUCTION ? [] : 'http://localhost:3001',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -27,4 +28,4 @@ import sampleRouter from './controller/v1/sampleController'
   app.use(sampleRouter)
 
   app.listen(port, () => logger.info(`Application listening on port ${PORT}!`))
-})()
+})
