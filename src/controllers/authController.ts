e3c05635d891c8032fcaa53f9239417bcdd931ba
Muhammad Sulthan Mazaya <73002733+MSMazaya@users.noreply.github.com>
2021-06-30 13:07:36 +0700

    refactor services

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,29 +1,20 @@
 import { Request, Response } from 'express'
-import * as userService from '../service/userService'
 import * as authService from '../service/authService'
 
 const login = async (req: Request, res: Response) => {
-  let user, passwordEncrypted;
   try {
-    user = await userService.getUserById(req.params.id);
+    const loginResult = await authService.handleLogin(req);
+    return res.status(200).json(loginResult);
   } catch (err) {
     return res.status(400).json(err);
   }
-
-  try {
-    passwordEncrypted = await authService.checkPassword(req.body.password, user.password.toString())
-    const accessToken = authService.generateAccessToken(user);
-    return res.status(200).json(user)
-  } catch (err) {
-    return res.status(400).json(err);
-  }
+  
 }
 
 const signup = async (req: Request, res: Response) => {
   try {
-    const user = await userService.createUser(req.body);
-    const accessToken = authService.generateAccessToken(user);
-    return res.status(200).json({user, accessToken});
+    const signUpResult = await authService.handleSignup(req);
+    return res.status(200).json(signUpResult); 
   } catch (err) {
     return res.status(400).json(err);
   }
--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,6 +1,35 @@
 import * as bcrypt from 'bcrypt'
+import { Request } from 'express';
 import * as jwt from 'jsonwebtoken'
+import * as userService from '../service/userService'
 import { User } from '../models/User'
+
+const handleLogin = async (req:Request) => {
+	let user, passwordEncrypted;
+	try {
+		user = await userService.getUserById(req.params.id);
+	} catch (err) {
+		return err;
+	}
+
+	try {
+		passwordEncrypted = await checkPassword(req.body.password, user.password.toString())
+		const accessToken = generateAccessToken(user);
+		return {user, accessToken};
+	} catch (err) {
+		return err;
+	}
+}
+
+const handleSignup = async (req:Request) => {
+	try {
+		const user = await userService.createUser(req.body);
+		const accessToken = generateAccessToken(user);
+		return {user, accessToken}
+	} catch (err) {
+		return err
+	}
+}
 
 const checkPassword = async (userPassword:string, reqPassword:string) : Promise<Boolean> => {
 	try{
@@ -15,4 +44,4 @@ const generateAccessToken = (user:User) : String => {
 	return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" })
 }
 
-export {checkPassword, generateAccessToken}
+export {handleLogin, handleSignup, checkPassword, generateAccessToken}
